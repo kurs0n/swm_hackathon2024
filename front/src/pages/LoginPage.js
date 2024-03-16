59c477111de24bab990847031bdb394c63f25451
Patryk Kurek <patryk744@op.pl>
2024-03-16 14:51:08 +0100

    feat: login and register connected

--- front/src/pages/LoginPage.js
+++ front/src/pages/LoginPage.js
@@ -9,39 +9,72 @@ function LoginPage() {
 
     const navigate = useNavigate();
 
-    const onLogin = (e, username, password) => {
+    const onLogin = async (e, username, password) => {
         e.preventDefault();
-        request("POST",
-            "/login",
-            {login: username, password: password}
-            ).then((response) => {
-                navigate("/mainapp");
-                setAuthToken(response.data.token);
-        }).catch((error) => {
-            console.error(error)
-            navigate("/")
-        });
+        try {
+            const res = await axios.post("http://localhost:8080/users/login",{
+                username: username,
+                password: password 
+            },{headers:{'Content-Type': 'application/json'}});
+
+            localStorage.setItem("access_token",res.data.access_token);
+            navigate("/mainapp");
+        }
+        catch(err){
+            console.error(err);
+            navigate("/");
+        }
+        // request("POST",
+        //     "http://localhost:8080/users/login",
+        //     {username: username, password: password}
+        //     ).then((response) => {
+        //         navigate("/mainapp");
+        //         console.log(response.data.token);
+        //         setAuthToken(response.data.token);
+        // }).catch((error) => {
+        //     console.error(error)
+        //     navigate("/")
+        // });
     };
 
-    const onRegister = (e, firstName, lastName, username, password) => {
+    const onRegister = async(e, firstName, lastName, username, password) => {
         e.preventDefault();
-        window.localStorage.removeItem("auth_token");
-        delete axios.defaults.headers.common['Authorization'];
-        request("POST",
-            "/register",
-            {
-                firstName: firstName,
-                lastName: lastName,
-                login: username,
+        window.localStorage.removeItem("access_token");
+        try{
+            const res = await axios.post("http://localhost:8080/users",{ 
+                firstname: firstName, 
+                lastname: lastName,
+                username: username,
                 password: password
-            }
-        ).then((response) => {
-            navigate("/mainapp")
-            setAuthToken(response.data.token);
-        }).catch((error) => {
-            console.error(error)
-            navigate("/")
-        });
+            },{headers: {'Content-Type': 'application/json'}});
+            
+            alert("Succesfully created user!");
+            navigate("/");
+        }
+        catch(err){
+            console.error(err);
+            navigate("/");
+        }
+
+
+
+        // delete axios.defaults.headers.common['Authorization'];
+        // request("POST",
+        //     "http://localhost:8080/users",
+        //     {
+        //         firstName: firstName,
+        //         lastName: lastName,
+        //         username: username,
+        //         password: password
+        //     }
+        // ).then((response) => {
+        //     navigate("/");
+        //     // navigate("/mainapp")
+        //     // setAuthToken(response.data.token);
+        // }).catch((error) => {
+        //     console.error(error)
+        //     navigate("/")
+        // });
     };
 
 
